80c3ba2d4db30e0f09f2806b47fdff6baba4da95
miray000 <88680089+Miray000@users.noreply.github.com>
2021-09-01 00:08:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $("button").click(function(){
+        $("body").toggleClass("black");
+        $("button").toggleClass("but");
+        $("header").toggleClass("head");
+       $("b").toggleClass("beder");
+       
+    });
+});
