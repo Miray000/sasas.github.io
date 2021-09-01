36f335a4b09f707a8ad7b37991e3fc21b74cb73a
miray000 <88680089+Miray000@users.noreply.github.com>
2021-09-01 11:29:29 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("button").click(function(){
-        $("body").toggleClass("black");
-        $("button").toggleClass("but");
-        $("header").toggleClass("head");
-       $("b").toggleClass("beder");
-       
-    });
-});
